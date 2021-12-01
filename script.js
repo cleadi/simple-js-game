65cbc9bf8c425a9352690e47fa3892056d1f9e2d
Dillon Cleaver <cleadi@fastmail.com>
2021-11-30 19:23:42 -0600

    Finished build.

---
+++ script.js
@@ -0,0 +1,25 @@
+let character = document.getElementById("character");
+let block = document.getElementById("block");
+
+jump = () => {
+  if (character.classList != "animate") {
+    character.classList.add("animate");
+  }
+  setTimeout(function () {
+    character.classList.remove("animate");
+  }, 500);
+};
+
+let checkGameOver = setInterval(function () {
+  let characterTop = parseInt(
+    window.getComputedStyle(character).getPropertyValue("top")
+  );
+  let blockLeft = parseInt(
+    window.getComputedStyle(block).getPropertyValue("left")
+  );
+  if (blockLeft < 20 && blockLeft > 0 && characterTop >= 130) {
+    block.style.animation = "none";
+    block.style.display = "none";
+    alert("OH NO! You lose.");
+  }
+}, 10);
